5dd47449b11322af8cc9ab9d485be6748c0cfa87
Edivaldo16 <edivaldojunior18@gmail.com>
2021-09-21 21:59:33 -0300

    Projeto Catalogo de Jogos

---
+++ ApiCatalogoJogos/ApiCatalogoJogos/InputModel/JogoInputModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCatalogoJogos.InputModel
+{
+    public class JogoInputModel
+    {
+        [Required]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "O nome do jogo deve conter entre 3 a 100 caracteres")]
+        public string Nome { get; set; }
+        [Required]
+        [StringLength(100, MinimumLength = 1, ErrorMessage = "O nome da produtora deve conter entre 1 a 100 caracteres")]
+        public string Produtora { get; set; }
+        [Required]
+        [Range(1, 1000, ErrorMessage = "O preço deve ser entre 1 R$ a 1000 R$")]
+        public double Preco { get; set; }
+    }
+}
